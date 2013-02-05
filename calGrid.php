084f15f48ea3feb6646c4cb3cfe2b43a72919287
fzhan <fzhan@sfu.ca>
2013-02-05 13:44:11 -0800

    add grid to gmap

---
+++ calGrid.php
@@ -0,0 +1,112 @@
+<?php
+require __DIR__.'/predis/autoload.php';
+$LAB = array(49.276802, -122.914913);
+$ROBOT = array("cb18", "cb01", "pi01");
+$single_server = array(
+    'host'	=>	'192.168.1.120',
+    'port'	=>	6379
+);
+$client = new Predis\Client($single_server);
+switch($_GET["type"])
+{
+case "energy":
+	$last_frame = explode(" ", $client->get("last_energy_frame"));
+	$last_color = explode(" ", $client->get("last_energy_color"));
+	$last = explode(" ", $client->get("last_energy"));
+	break;
+case "time":
+	$last_frame = explode(" ", $client->get("last_time_frame"));
+	$last_color = explode(" ", $client->get("last_time_color"));
+	$last = explode(" ", $client->get("last_time"));
+	break;
+default:
+	$last_frame = "";
+	$last_color = "";
+	$last = "";
+}
+$map = array();
+for($i = 0; $i + 2 < count($last); $i += 3)
+{
+	$map[$last[$i]." ".$last[$i+1]] = floatval($last[$i+2]);
+}
+$old_color = array();
+for($i = 0; $i + 2 < count($last_color); $i += 3)
+{
+	$old_color[$last_color[$i]." ".$last_color[$i+1]] = floatval($last_color[$i+2]);
+}
+for ($i = 0; $i < count($ROBOT); ++ $i)
+{
+	$r = explode(" ", $client->get($ROBOT[$i]));
+	$now = intval($r[0]);
+	if (count($last_frame) < count($ROBOT) || $last_frame[$i] >= $now)
+	{
+		$last_frame[$i] = $now;
+		continue;
+	}
+	$x = round((floatval($r[1]) - $LAB[0] - $_GET["grid_size"]/2) / $_GET["grid_size"]);
+	$y = round((floatval($r[2]) - $LAB[1] - $_GET["grid_size"]/2) / $_GET["grid_size"]);
+	switch($_GET["type"])
+	{
+	case "energy":
+		$value = floatval($r[3]) * floatval($r[4]) * ($now - $last_frame[$i]);
+		break;
+	case "time":
+		$value = $now - $last_frame[$i];
+		break;
+	default:
+		$value = 0;
+	}
+	if (array_key_exists($new_x." ".$new_y, $map))
+	{
+		$map[$x." ".$y] += $value;
+	} else
+	{
+		$map[$x." ".$y] = $value;
+	}
+}
+switch($_GET["type"])
+{
+case "energy":
+	$client->set("last_energy_frame", implode(" ", $last_frame));
+	break;
+case "time":
+	$client->set("last_time_frame", implode(" ", $last_frame));
+	break;
+default:
+}
+$sum = array_sum($map);
+$last = "";
+foreach ($map as $key => $i)
+{
+	$last .= $key." ".$i." ";
+	$i = $i / $sum;
+	if ($i < 0.01)
+	{
+		$color = 230;
+	}
+	else if ($i > 0.3)
+	{
+		$color = 50;
+	} else
+	{
+		$color = round((230 - 50) * ($i - 0.01) / (0.3 - 0.01) + 50);
+	}
+	if (array_key_exists($key, $old_color) || $old_color[$key] != $color)
+	{
+		$old_color[$key] = $color;
+	}
+	echo $key." ".$color." ";
+}
+switch($_GET["type"])
+{
+case "energy":
+	$client->set("last_energy", $last);
+	$client->set("last_energy_color", $old_color);
+	break;
+case "time":
+	$client->set("last_time", $last);
+	$client->set("last_time_color", $old_color);
+	break;
+default:
+}
+?>
---
+++ set_redis.php
@@ -0,0 +1,72 @@
+<?php
+// by fzhan@Autolab
+require __DIR__.'/predis/autoload.php';
+
+$LAB = array(49.276802, -122.914913);
+$OFFSET = 0.0005;
+$begin = microtime(true);
+$single_server = array(
+    'host'	=>	$_GET["host"],
+    'port'	=>	$_GET["port"]
+);
+$client = new Predis\Client($single_server);
+
+function random_value()
+{
+	return rand(0, 10000) / 100;
+}
+function random_robot($client_value)
+{
+	$c = explode(" ", $client_value);
+	// the time stamp of last cycle
+	$last_time = intval($c[0]);
+	// there is no previous values
+	if (count($c) < 3)
+	{
+		// the location of the lab
+		$c[1] = 49.276802;
+		$c[2] = -122.914913;
+	}
+	$x = floatval($c[1]) + rand(0, 10000) / 10000 * 0.0005 - 0.0005 / 2;
+	$y = floatval($c[2]) + rand(0, 10000) / 10000 * 0.0005 - 0.0005 / 2;
+	return ($last_time+1)." ".$x." ".$y." ".(rand(0, 10000) / 100)." ".(rand(0, 10000) / 100);
+}
+
+$client->set("delay_test", $_GET["time"]);
+echo "time ".$client->get("delay_test").", ";
+//echo round(microtime(true) * 1000 - $_GET["time"]);
+
+// suppose there are 10 robots
+for($i = 0; $i < 10; ++ $i)
+{
+	// if the key is valid
+	if($_GET["key".$i] != undefined && $_GET["key".$i] != null && $_GET["key".$i] != "")
+	{
+		$value = $_GET["value".$i];
+		// if it is the code
+		if (substr($_GET["value".$i], 0, 3) == "@@@")
+		{
+			$code = substr($_GET["value".$i], 3);
+			switch($code)
+			{
+			case "clear":
+				$value = "";
+				break;
+			case "random_value":
+				$value = random_value();
+				break;
+			case "random_robot":
+				$value = random_robot($client->get($_GET["key".$i]));
+				break;
+			default:
+				// undefined code
+				$value = $code;
+				break;
+			}
+		}
+		$client->set($_GET["key".$i], $value);
+		echo $_GET["key".$i].": ".$client->get($_GET["key".$i]).", ";
+	}
+}
+//echo "delay in php: ".round((microtime(true) - $begin) * 1000);
+?>
